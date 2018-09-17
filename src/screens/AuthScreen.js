a54de8390236acd2a791d89176ce9ee3e25b6d82
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2018-09-17 22:29:54 +0530

    AuthScreen: Designing part complete

---
+++ src/screens/AuthScreen.js
@@ -0,0 +1,379 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  StatusBar,
+} from 'react-native';
+import { Font, LinearGradient } from 'expo';
+import { Card, Icon } from 'react-native-elements';
+import { createIconSetFromIcoMoon } from '@expo/vector-icons';
+
+import icoMoonConfig from '../../assets/selection.json';
+import styles from '../styles/AuthScreenStyle';
+
+const Iconset = createIconSetFromIcoMoon(icoMoonConfig, 'icomoon');
+
+class AuthScreen extends Component {
+  state = {
+    fontLoaded: false,
+    appIcon: '',
+    facebookIcon: '',
+    googleIcon: '',
+    successIcon: '',
+    email: '',
+    password: '',
+    confirmpassword: '',
+    name: '',
+    phone: '',
+    chosenCard: 'login',
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'poppins-bold': require('../../assets/fonts/Poppins-Bold.otf'),
+      'poppins-extralight': require('../../assets/fonts/Poppins-ExtraLight.otf'),
+      'poppins-light': require('../../assets/fonts/Poppins-Light.otf'),
+      'poppins-regular': require('../../assets/fonts/Poppins-Regular.otf'),
+      'poppins-medium': require('../../assets/fonts/Poppins-Medium.otf'),
+      icomoon: require('../../assets/fonts/icomoon.ttf'),
+    });
+
+    const appIcon = await require('../../assets/icons/app-icon.png');
+    const facebookIcon = await require('../../assets/icons/facebook.png');
+    const googleIcon = await require('../../assets/icons/google.png');
+    const successIcon = await require('../../assets/icons/success.png');
+
+    this.setState({
+      fontLoaded: true,
+      appIcon,
+      facebookIcon,
+      googleIcon,
+      successIcon,
+    });
+  }
+
+  LoginCard() {
+    return (
+      <Card containerStyle={styles.AuthCard}>
+        <View style={styles.SocialButtons}>
+          <TouchableOpacity>
+            <Image
+              source={this.state.facebookIcon}
+              resizeMode="center"
+              style={styles.FacebookButton}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Image source={this.state.googleIcon} resizeMode="center" style={styles.GoogleButton} />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.LoginDivider}>
+          <Text style={styles.LoginDividerLine}>─────</Text>
+          <Text style={styles.LoginDividerText}>&nbsp; OR &nbsp;</Text>
+          <Text style={styles.LoginDividerLine}>─────</Text>
+        </View>
+        <View style={styles.Form}>
+          <Iconset name="email" size={15} />
+          <TextInput
+            style={[styles.FormInput, { marginLeft: 14 }]}
+            onChangeText={text => this.setState({ email: text })}
+            value={this.state.email}
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="while-editing"
+            keyboardType="email-address"
+            placeholder="username@example.com"
+            textContentType="emailAddress"
+            underlineColorAndroid="rgba(0,0,0,0)"
+          />
+        </View>
+        <View style={styles.Form}>
+          <Iconset name="password" size={20} />
+          <TextInput
+            style={styles.FormInput}
+            onChangeText={text => this.setState({ password: text })}
+            value={this.state.password}
+            secureTextEntry
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="while-editing"
+            placeholder="password"
+            textContentType="password"
+            underlineColorAndroid="rgba(0,0,0,0)"
+          />
+        </View>
+        <TouchableOpacity
+          style={styles.Button}
+          onPress={() => this.props.navigation.navigate('main')}>
+          <Text style={styles.ButtonText}>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.ForgotPass}
+          onPress={() => this.setState({ chosenCard: 'forgot' })}>
+          <Text style={styles.ForgotPassText}>Forgot Password?</Text>
+        </TouchableOpacity>
+      </Card>
+    );
+  }
+
+  ForgotPasswordCard() {
+    return (
+      <Card containerStyle={styles.AuthCard}>
+        <Text style={styles.ForgotPasswordCardText}>
+          Enter your email address and we will send you a link to reset your password.
+        </Text>
+        <View style={[styles.Form, { marginTop: 50 }]}>
+          <Iconset name="email" size={15} />
+          <TextInput
+            style={[styles.FormInput, { marginLeft: 14 }]}
+            onChangeText={text => this.setState({ email: text })}
+            value={this.state.email}
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="while-editing"
+            keyboardType="email-address"
+            placeholder="Registered Email Address"
+            textContentType="emailAddress"
+            underlineColorAndroid="rgba(0,0,0,0)"
+          />
+        </View>
+        <TouchableOpacity
+          style={[styles.Button, { marginTop: 80 }]}
+          onPress={() => this.setState({ chosenCard: 'reset' })}>
+          <Text style={styles.ButtonText}>SEND LINK</Text>
+        </TouchableOpacity>
+      </Card>
+    );
+  }
+
+  ResetPasswordCard() {
+    return (
+      <Card containerStyle={styles.AuthCard}>
+        <View style={styles.ResetPasswordHeading}>
+          <TouchableOpacity style={{ marginLeft: 10 }}>
+            <Icon name="close" color="#090909" type="evilicons" size={20} />
+          </TouchableOpacity>
+          <Text style={styles.ResetPasswordHeadingText}>Reset Password</Text>
+        </View>
+
+        <View style={[styles.Form, { marginTop: 50 }]}>
+          <TextInput
+            style={[styles.FormInput, { marginLeft: 0 }]}
+            onChangeText={text => this.setState({ password: text })}
+            value={this.state.password}
+            secureTextEntry
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="while-editing"
+            placeholder="Enter New Password"
+            textContentType="password"
+            underlineColorAndroid="rgba(0,0,0,0)"
+          />
+        </View>
+        <View style={styles.Form}>
+          <TextInput
+            style={[styles.FormInput, { marginLeft: 0 }]}
+            onChangeText={text => this.setState({ confirmpassword: text })}
+            value={this.state.confirmpassword}
+            secureTextEntry
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="while-editing"
+            placeholder="Re-type New Password"
+            textContentType="password"
+            underlineColorAndroid="rgba(0,0,0,0)"
+          />
+        </View>
+        <TouchableOpacity
+          style={[styles.Button, { marginTop: 60 }]}
+          onPress={() => this.setState({ chosenCard: 'resetsuccess' })}>
+          <Text style={styles.ButtonText}>CHANGE PASSWORD</Text>
+        </TouchableOpacity>
+      </Card>
+    );
+  }
+
+  ResetPasswordSuccessCard() {
+    return (
+      <Card containerStyle={styles.AuthCard}>
+        <View style={styles.ResetPasswordSuccessCard}>
+          <Image
+            source={this.state.successIcon}
+            resizeMode="contain"
+            style={styles.ResetPasswordSuccessCardIcon}
+          />
+        </View>
+        <Text style={styles.ResetPasswordSuccessCardText}>
+          Your password has been successfully changed!
+        </Text>
+        <TouchableOpacity
+          style={[styles.Button, { marginTop: 60 }]}
+          onPress={() => this.setState({ chosenCard: 'login' })}>
+          <Text style={styles.ButtonText}>LOGIN</Text>
+        </TouchableOpacity>
+      </Card>
+    );
+  }
+
+  SignupCard() {
+    return (
+      <Card containerStyle={styles.AuthCard}>
+        <View style={styles.Form}>
+          <Iconset name="username" size={20} />
+          <TextInput
+            style={styles.FormInput}
+            onChangeText={text => this.setState({ name: text })}
+            value={this.state.name}
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="while-editing"
+            keyboardType="default"
+            placeholder="Name"
+            textContentType="name"
+            underlineColorAndroid="rgba(0,0,0,0)"
+          />
+        </View>
+        <View style={styles.Form}>
+          <Iconset name="phone" size={20} />
+          <TextInput
+            style={styles.FormInput}
+            onChangeText={text => this.setState({ phone: text })}
+            value={this.state.phone}
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="while-editing"
+            keyboardType="numeric"
+            placeholder="Contact Number"
+            textContentType="telephoneNumber"
+            underlineColorAndroid="rgba(0,0,0,0)"
+          />
+        </View>
+        <View style={styles.Form}>
+          <Iconset name="email" size={15} />
+          <TextInput
+            style={[styles.FormInput, { marginLeft: 14 }]}
+            onChangeText={text => this.setState({ email: text })}
+            value={this.state.email}
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="while-editing"
+            keyboardType="email-address"
+            placeholder="username@example.com"
+            textContentType="emailAddress"
+            underlineColorAndroid="rgba(0,0,0,0)"
+          />
+        </View>
+        <View style={styles.Form}>
+          <Iconset name="password" size={20} />
+          <TextInput
+            style={styles.FormInput}
+            onChangeText={text => this.setState({ password: text })}
+            value={this.state.password}
+            secureTextEntry
+            autoCapitalize="none"
+            autoCorrect={false}
+            clearButtonMode="while-editing"
+            placeholder="password"
+            textContentType="password"
+            underlineColorAndroid="rgba(0,0,0,0)"
+          />
+        </View>
+        <TouchableOpacity
+          style={styles.Button}
+          onPress={() => this.props.navigation.navigate('main')}>
+          <Text style={styles.ButtonText}>SIGN UP</Text>
+        </TouchableOpacity>
+      </Card>
+    );
+  }
+
+  displayCard() {
+    switch (this.state.chosenCard) {
+      case 'login':
+        return this.LoginCard();
+      case 'signup':
+        return this.SignupCard();
+      case 'forgot':
+        return this.ForgotPasswordCard();
+      case 'reset':
+        return this.ResetPasswordCard();
+      case 'resetsuccess':
+        return this.ResetPasswordSuccessCard();
+      default:
+        return this.LoginCard();
+    }
+  }
+
+  render() {
+    return (
+      <KeyboardAvoidingView style={[styles.Back, { flex: 1 }]} behavior="position" enabled>
+        <StatusBar hidden />
+        {this.state.fontLoaded ? (
+          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <View style={styles.Back}>
+              <View style={styles.Back}>
+                <View style={styles.TopBack}>
+                  <LinearGradient
+                    colors={['#990003', '#CF0003']}
+                    style={{ flex: 1 }}
+                    start={[0, 1]}
+                    end={[0.75, 1]}
+                  />
+                </View>
+                <View style={styles.MiddleBack} />
+                <View style={styles.BottomBack} />
+              </View>
+              <View style={styles.Front}>
+                <Image source={this.state.appIcon} resizeMode="contain" style={styles.AppIcon} />
+                <View style={styles.Headings}>
+                  <TouchableOpacity
+                    style={styles.LoginSelection}
+                    onPress={() => this.setState({ chosenCard: 'login' })}>
+                    <Text
+                      style={[
+                        styles.LoginSelectionText,
+                        {
+                          color:
+                            this.state.chosenCard === 'login'
+                              ? '#FFFFFF'
+                              : 'rgba(256,256,256,0.56)',
+                        },
+                      ]}>
+                      Login
+                    </Text>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity
+                    style={styles.SignupSelection}
+                    onPress={() => this.setState({ chosenCard: 'signup' })}>
+                    <Text
+                      style={[
+                        styles.SignupSelectionText,
+                        {
+                          color:
+                            this.state.chosenCard === 'signup'
+                              ? '#FFFFFF'
+                              : 'rgba(256,256,256,0.56)',
+                        },
+                      ]}>
+                      Signup
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View>{this.displayCard()}</View>
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        ) : null}
+      </KeyboardAvoidingView>
+    );
+  }
+}
+
+export default AuthScreen;
---
+++ src/styles/AuthScreenStyle.js
@@ -0,0 +1,192 @@
+import { Platform, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  Back: {
+    height: '100%',
+    backgroundColor: '#F5F5F5',
+    zIndex: 0,
+  },
+  TopBack: {
+    backgroundColor: '#FF5F35',
+    flex: 2,
+    borderWidth: 0,
+    zIndex: 1,
+  },
+  BackImage: {
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    opacity: 0.4,
+    zIndex: 2,
+  },
+  MiddleBack: {
+    backgroundColor: '#F5F5F5',
+    borderRadius: 50,
+    width: '11%',
+    height: 50,
+    transform: [{ scaleX: 10 }],
+    alignSelf: 'center',
+    zIndex: 1,
+    top: -20,
+  },
+  BottomBack: {
+    backgroundColor: '#F5F5F5',
+    flex: 2,
+    zIndex: 1,
+  },
+  Front: {
+    height: '100%',
+    zIndex: 3,
+    top: '-100%',
+  },
+  AppIcon: {
+    marginTop: 50,
+    height: '7%',
+    alignSelf: 'center',
+  },
+  Headings: {
+    marginTop: 50,
+    flexDirection: 'row',
+  },
+  LoginSelection: {
+    marginLeft: 30,
+    flex: 1,
+  },
+  SignupSelection: {
+    marginRight: 30,
+    flex: 1,
+  },
+  LoginSelectionText: {
+    fontFamily: 'poppins-bold',
+    fontSize: 20,
+    color: 'rgba(256,256,256,0.56)',
+  },
+  SignupSelectionText: {
+    fontFamily: 'poppins-bold',
+    fontSize: 20,
+    color: 'rgba(256,256,256,0.56)',
+    textAlign: 'right',
+  },
+  AuthCard: {
+    backgroundColor: '#FFFFFF',
+    borderWidth: 0,
+    height: 385,
+    borderRadius: 5,
+    shadowRadius: 0,
+    shadowOpacity: 0,
+    zIndex: 5,
+  },
+  SocialButtons: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  FacebookButton: {
+    height: 50,
+    width: 50,
+  },
+  GoogleButton: {
+    marginLeft: 50,
+    height: 50,
+    width: 50,
+  },
+  LoginDivider: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 5,
+  },
+  LoginDividerLine: {
+    fontFamily: 'poppins-light',
+    fontSize: 17,
+    textAlign: 'center',
+    color: '#979797',
+  },
+  LoginDividerText: {
+    fontFamily: 'poppins-light',
+    fontSize: 17,
+    textAlign: 'center',
+    color: '#000000',
+  },
+  Form: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    padding: 15,
+    borderWidth: 1,
+    borderColor: '#D3D4E5',
+  },
+  FormIcon: {
+    height: 20,
+    width: 20,
+  },
+  FormInput: {
+    flex: 10,
+    marginTop: Platform.OS === 'android' ? 5 : 0,
+    marginLeft: 15,
+    height: 20,
+    fontFamily: 'poppins-light',
+    fontSize: 15,
+  },
+  Button: {
+    marginTop: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    borderRadius: 5,
+    borderWidth: 0,
+    padding: 12,
+    backgroundColor: '#CF0003',
+  },
+  ButtonText: {
+    fontFamily: 'poppins-light',
+    fontSize: 17,
+    textAlign: 'center',
+    color: '#FFFFFF',
+  },
+  ForgotPass: {
+    marginTop: 15,
+  },
+  ForgotPassText: {
+    fontFamily: 'poppins-extralight',
+    fontSize: 15,
+    textAlign: 'center',
+    color: '#000000',
+  },
+  ForgotPasswordCardText: {
+    marginTop: 30,
+    fontFamily: 'poppins-light',
+    fontSize: 14,
+    textAlign: 'center',
+    color: '#979797',
+  },
+  ResetPasswordHeading: {
+    marginTop: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  ResetPasswordHeadingText: {
+    fontFamily: 'poppins-light',
+    fontSize: 16,
+    marginLeft: 10,
+    color: '#000000',
+  },
+  ResetPasswordSuccessCard: {
+    marginTop: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  ResetPasswordSuccessCardIcon: {
+    height: '45%',
+  },
+  ResetPasswordSuccessCardText: {
+    fontFamily: 'poppins-light',
+    fontSize: 14,
+    textAlign: 'center',
+    color: '#000000',
+  },
+});
+
+export default styles;
